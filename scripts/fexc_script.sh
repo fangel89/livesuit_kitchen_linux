4b3fc4ed554cebe5d1fa3ae7ae07751a34a7b2e8
user <user@comp1.lan>
2013-03-16 14:32:42 +0200

    Initital commit

--- scripts/fexc_script.sh
+++ scripts/fexc_script.sh
@@ -3,7 +3,7 @@ if [ "$1" = 1 ]
 then
   if [ -e ./_temp/bootfs/script.bin ]
   then
-    sudo ./tools/sunxitools/bin2fex ./_temp/bootfs/script.bin ./_temp/bootfs/script.ini
+    sudo ./tools/sunxi_tools/bin2fex ./_temp/bootfs/script.bin ./_temp/bootfs/script.ini
     sudo rm ./_temp/bootfs/script*.bin
   else
     echo -en "\tFile script.bin not found!"
@@ -11,7 +11,7 @@ then
 else
   if [ -e ./_temp/bootfs/script.ini ]
   then
-    sudo ./tools/sunxitools/fex2bin ./_temp/bootfs/script.ini ./_temp/bootfs/script.bin
+    sudo ./tools/sunxi_tools/fex2bin ./_temp/bootfs/script.ini ./_temp/bootfs/script.bin
     sudo cp -p ./_temp/bootfs/script.bin ./_temp/bootfs/script0.bin
     sudo rm ./_temp/bootfs/script.ini
   else
